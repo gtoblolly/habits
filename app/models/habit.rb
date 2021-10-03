f9e1966b0714df78976a5db26c0cab31bf9269dd
kengo <zsensikengo@gmail.com>
2021-10-04 06:55:51 +0900

    アソシエーションの定義

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,5 +1,6 @@
 class Habit < ApplicationRecord
   has_many :records, dependent: :destroy
+  belongs_to :user
   with_options presence: true do
     validates :title
     validates :content
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,5 @@
 class Record < ApplicationRecord
   belongs_to :habit
+  belongs_to :user
   has_one_attached :image
 end
