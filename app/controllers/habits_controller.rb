e6634787f624e3c12862ac5656dabd2c4908318a
kengo <zsensikengo@gmail.com>
2021-10-13 19:31:50 +0900

    編集、削除昨日のアクセス制限

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,4 +1,5 @@
 class HabitsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @habits = Habit.all
@@ -42,6 +43,13 @@ class HabitsController < ApplicationController
 
   def habit_params
     params.require(:habit).permit(:title, :content).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @habit = Habit.find(params[:id])
+    unless user_signed_in? && current_user.id == @habit.user.id
+      redirect_to root_path
+    end
   end
 
 end
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,5 @@
 class RecordsController < ApplicationController
+  before_action :move_to_index, except: :show
 
   def new
     @record = Record.new
@@ -34,10 +35,31 @@ class RecordsController < ApplicationController
     redirect_to root_path
   end
 
-    private
+  private
 
-    def record_params
-      params.require(:record).permit(:text, :habit_id, :image).merge(user_id: current_user.id)
+  def record_params
+    params.require(:record).permit(:text, :habit_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @record = Record.find(params[:id])
+    unless user_signed_in? && current_user.id == @record.user.id
+      redirect_to root_path
     end
+  end
     
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
