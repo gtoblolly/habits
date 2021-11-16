9efac150495df7a4b645bc76a11a2fede405d14d
kengo <zsensikengo@gmail.com>
2021-11-16 09:10:55 +0900

    イイね機能へのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "habits#index"
   resources :habits do
-    resources :records, only: [:new, :create, :show, :destroy, :edit, :update]
+    resources :records, only: [:new, :create, :show, :destroy, :edit, :update] do
+      resource :likes, only: [:create, :destroy]
+    end
   end
   resources :users, only: :show
 end
