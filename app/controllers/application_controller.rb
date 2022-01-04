fc0877e781b26bc1f6304ea5624e6bb2651eafc6
kengo <zsensikengo@gmail.com>
2022-01-05 07:01:41 +0900

    環境変数の準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_google_calender
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def set_google_calender
+    gon.google_calender_API_key = ENV['GOOGLE_CALENDER_API_KEY']
   end
   
 end
