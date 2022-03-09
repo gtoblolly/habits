5e42d48c5aced43a97ec56d1ae58af7b5810629b
kengo <zsensikengo@gmail.com>
2022-03-09 18:08:10 +0900

    カレンダーファイルの移動

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../calendar")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,33 +16,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import googleCalendarApi from '@fullcalendar/google-calendar'
-
-document.addEventListener('turbolinks:load', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new Calendar(calendarEl, {
-    locale: 'ja',
-    dayCellContent: function(e) { e.dayNumberText = e.dayNumberText.replace('日', ''); },
-    buttonText: { today: '今日', },
-    plugins: [ dayGridPlugin, interactionPlugin, googleCalendarApi ],
-    googleCalendarApiKey: gon.google_calender_API_key,
-    eventSources: [
-      {
-        googleCalendarId: 'ja.japanese#holiday@group.v.calendar.google.com',
-        color:"#ffd0d0",
-        display: 'background'
-      },
-      {
-        url: '/habits.json',
-        color: 'green',
-        display: 'background'
-      },
-    ],
-  });
-
-  calendar.render();
-});
