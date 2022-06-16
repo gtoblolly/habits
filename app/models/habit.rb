081d7d2596d830ff73547ef7612f63337ba6023c
kengo <zsensikengo@gmail.com>
2022-06-17 06:58:13 +0900

    習慣モデルのバリデーション定義

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,8 +1,6 @@
 class Habit < ApplicationRecord
   has_many :records, dependent: :destroy
   belongs_to :user
-  with_options presence: true do
-    validates :title
-    validates :content
-  end
+
+  validates :title, presence: true, length: { maximum: 20 }
 end
