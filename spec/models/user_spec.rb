c8b63179956368571ef3cb054e5c5f3c1c162da5
kengo <zsensikengo@gmail.com>
2022-08-09 06:16:18 +0900

    userモデル単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,31 +8,68 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
       it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+        expect(@user).to be_valid
       end
       it "nicknameが31文字以下であれば登録できる" do
+        @user.nickname = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        expect(@user).to be_valid
       end
       it "passwordとpassword_confirmationが6文字以上であれば登録できる" do
+        @user.password = "000000"
+        @user.password_confirmation = "000000"
+        expect(@user).to be_valid
       end
     end
 
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前を入力してください")
       end
       it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
       end
       it 'passwordが空では登録できない' do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください", "パスワード（確認用）とパスワードの入力が一致しません")
       end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
       it 'nicknameが31文字以上では登録できない' do
+        @user.nickname = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前は30文字以内で入力してください")
       end
       it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
       end
       it 'emailは@を含まないと登録できない' do
+        @user.email = 'hoge'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールは不正な値です")
       end
       it 'passwordが5文字以下では登録できない' do
+        @user.password = "00000"
+        @user.password_confirmation = "00000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
       end
       it 'passwordが129文字以上では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 129)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは128文字以内で入力してください")
       end
     end
   end
