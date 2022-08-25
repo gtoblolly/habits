a041e3739c7a5f3822bbc1512f6aa77bc297b5ec
kengo <zsensikengo@gmail.com>
2022-08-26 06:54:55 +0900

    habitモデルのexampleを列挙

--- spec/factories/habits.rb
+++ spec/factories/habits.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :habit do
-    
+    title     {Faker::Lorem.sentence}
+    content   {Faker::Lorem.sentence}
+    level     {0}
+    exp_sum   {0}
+    association :user
   end
 end
--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Habit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @habit = FactoryBot.build(:habit)
+  end
+
+  describe '習慣の登録' do
+    context '習慣を登録できる場合' do
+      it '習慣名と習慣内容を登録できる' do
+      end
+      it '習慣名のみでも登録できる' do
+      end
+      it "習慣名がが21文字以下であれば登録できる" do
+      end
+    end
+
+    context '習慣が登録できない場合' do
+      it '習慣名が空では登録できない' do
+      end
+      it 'ユーザーが紐付いていなければ登録できない' do
+      end
+      it '習慣名が21文字以上では登録できない' do
+      end
+    end
+  end
+
 end
