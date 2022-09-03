3ef0cf7d0e20a080eaf51681f58cd3aaf48a7f34
kengo <zsensikengo@gmail.com>
2022-09-04 06:59:03 +0900

    likeモデルのexampleを列挙

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+
+  describe "いいね機能" do
+    context "いいねできる場合" do
+      it "ユーザーと記録が紐づいていれば保存できる" do
+      end
+      it "ユーザーが同じでも記録が違えばいいねできる" do
+      end
+      it "記録が同じでもユーザーが違えばいいねできる" do
+      end
+    end
+    context 'いいねできない場合' do
+      it "ユーザーが紐付いていなければ投稿できない" do
+      end
+      it "記録が紐付いていなければ投稿できない" do
+      end
+    end
+  end
 end
