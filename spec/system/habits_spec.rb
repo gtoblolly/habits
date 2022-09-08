13d43cbbf9cb9502db2a4fd494ab8ec4783092d7
kengo <zsensikengo@gmail.com>
2022-09-09 06:02:04 +0900

    習慣生成のexample挙動洗い出し

--- spec/system/habits_spec.rb
+++ spec/system/habits_spec.rb
@@ -2,8 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Habits", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @habit_title = Faker::Lorem.sentence
+    @habit_content = Faker::Lorem.sentence
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context '習慣を生成できるとき'do
+    it 'ログインしたユーザーは習慣を生成できる' do
+      # ログインする
+      # マイページへのボタンがあることを確認する
+      # マイページに移動する
+      # 習慣生成ページへのボタンがあることを確認する
+      # 習慣生成ページに移動する
+      # フォームに情報を入力する
+      # 送信するとHabitモデルのカウントが1上がることを確認する
+      # マイページに遷移することを確認する
+      # マイページには先ほど生成した習慣が存在することを確認する
+    end
+  end
+  context '習慣生成ができないとき'do
+    it 'ログインしていないとページに遷移できない' do
+      # トップページに遷移する
+      # マイページへのボタンがないことを確認する
+    end
+  end
 end
